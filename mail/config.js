98f478f70d326e37fc58b89d9aaca2c5b92c3505
BikashJhendi <bikashjhendi11@gmail.com>
2021-08-10 19:14:03 +0545

    oauth token renew

--- mail/config.js
+++ mail/config.js
@@ -3,6 +3,6 @@ module.exports = {
     CLIENT_ID: '304854182637-oj1o6r5aqc9hanl5s96oagjith4vvu8i.apps.googleusercontent.com',
     CLIENT_SECRET: 'PRQ-tZFu0ik8Lh-CeTdzizFZ',
     REDIRECT_URI: 'https://developers.google.com/oauthplayground',
-    REFRESH_TOKEN: '1//04E9PAPzsNugRCgYIARAAGAQSNwF-L9Ir1lKdUD9aEOBip-fGFpldo90clvUKeAdZ4nqF3lXYXoZHmtR2i4qO2xYE3ilUGuEBVCw',
+    REFRESH_TOKEN: '1//04LMDdB2bi1sBCgYIARAAGAQSNwF-L9Ir2LKLZYLCRQxH_PH3ec9g-R8oLK0SvIqi46ZZZgBaRr60FHGC8HwI6EfrSjrZFU4pZbs',
     FRONTEND_HOST: 'http://localhost:3000'
 }
