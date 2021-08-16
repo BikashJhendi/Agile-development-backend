31704aa9c9b4876a0d3bb6b0fa5961d111a93487
BikashJhendi <bikashjhendi11@gmail.com>
2021-08-16 22:03:28 +0545

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const localHostUrl = 'mongodb://127.0.0.1:27017/DropShipping'
 
-mongoose.connect(process.env.MONGODB_URL || localHostUrl, {
+mongoose.connect('mongodb+srv://dhuwani:DzlFUD3pbRQWkJnl@dhuwani.jakvu.mongodb.net/dhuwaniDB?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
