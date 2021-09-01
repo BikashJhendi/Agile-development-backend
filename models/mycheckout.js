4803b5eee8b6574ad682ff9bd89f0b475b12e94c
AnilSubediii <anilsubedi331@gmail.com>
2021-09-01 23:55:07 +0545

    routed updated

--- models/mycheckout.js
+++ models/mycheckout.js
@@ -33,7 +33,7 @@ const mycheckout = mongoose.model('mycheckout', {
         },
 
     },
-
+    
     billingaddress: {
         billingfirstname: {
             type: String
--- routes/productReviewRoute.js
+++ routes/productReviewRoute.js
@@ -1,26 +1,73 @@
 const express = require('express');
-const reviewupload = require('../middleware/review');
+const reviewupload = require('../middleware/ImageController/review');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const productReview = require('../models/productReview');
+const { check, validationResult } = require('express-validator');
+const uploadReviewImg = require('../middleware/ImageController/review');
+const multer = require('multer');
 
-router.post('/product/review', reviewupload.single('reviewImage'), function (req, res) {
+const maxImage = 5;
+var upload = uploadReviewImg.array('reviewImages', maxImage)
 
-    const productId = req.body.productId;
-    const review = req.body.review;
-    const rating = req.body.rating;
-    
 
-    const review_data = new productReview({ productId: productId, review: review,  rating: rating,  reviewImage:req.file.filename });
-
-    review_data.save()
-        .then(function (result) {
-            res.status(201).json({ message: "Review Added" })
-        })
-        .catch(function (err) {
-            res.status(500).json({
+router.post('/product/review', function (req, res) {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            return res.status(400).json({
+                message: "A Multer error occurred when uploading",
                 success: false,
-                message: err })
-        });
+                err: err
+            })
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            return res.status(400).json({
+                message: "An unknown error occurred when uploading",
+                success: false,
+                err: err
+            })
+        }
+        const errors = validationResult(req);
+        console.log(errors.array())
+        const token = req.headers.authorization.split(" ")[1];
+        const decode = jwt.verify(token, "secretKey");
+        const { firstName, lastName } = decode;
+
+        const { productId, review, rating } = req.body;
+
+        if (!req.files || req.files.length == 0) {
+            return res.status(400).json({
+                message: "Require at least one image.",
+                success: false
+            })
+        }
+        else {
+
+            // creating array to store image filename
+            const arrayOfImg = new Array();
+
+            // requesting the length of files and adding it into the array of list.
+            for (i = 0; i < req.files.length; i++) {
+                // adding image filename to the array. [hero, car, bike, ...]
+                arrayOfImg.push({ imageName: req.files[i].filename })
+            }
+
+
+            const review_data = new productReview({ productId, firstName, lastName, review, rating, reviewImages: arrayOfImg });
+
+            review_data.save()
+                .then(function (result) {
+                    res.status(201).json({ message: "Review Added" })
+                })
+                .catch(function (err) {
+                    res.status(500).json({
+                        success: false,
+                        message: err
+                    })
+                });
+        }
+    })
 })
 
 
@@ -35,10 +82,11 @@ router.get('/productone/review/:productId', function (req, res) {
             })
         })
         .catch(function (e) {
-           
+
             res.status(500).json({
                 success: false,
-                message: e })
+                message: e
+            })
         });
 })
 
